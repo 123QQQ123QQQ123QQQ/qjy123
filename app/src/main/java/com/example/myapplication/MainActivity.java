8088533184b65572fdec366e7c3674f705e21df3
123QQQ123QQQ123QQQ <2331525413@qq.com>
2022-03-20 22:31:47 +0800

    commit message

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
